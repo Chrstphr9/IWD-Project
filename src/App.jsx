f46e4e6ed2b77f14737ec88fe453055d87117496
somto <somto565@gmail.com>
2024-02-25 11:48:39 +0100

    home page ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Login from './pages/Login'
 import SIgnUp from './pages/SIgnUp'
+import Home from './pages/Home'
 
 const App = () => {
   return (
     <div>
     <Login />
     <SIgnUp />
+    <Home />
     </div>
   )
 }
