3dceda7c877655ade2d75b9c1bbf0e4feb021eaa
somto <somto565@gmail.com>
2024-02-26 00:46:34 +0100

    done with auth

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,46 @@ import React from 'react'
 import Login from './pages/Login'
 import SIgnUp from './pages/SIgnUp'
 import Home from './pages/Home'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 
 const App = () => {
+
+  const currentUser = true;
+
+  const RequireAuth = ({children}) => {
+    return currentUser ? (children) : <Navigate to="/login"/>
+  };
+
+  
   return (
+    // <div>
+    // <Login />
+    
+    // {/* <Home /> */}
+    // </div>
+    // <div>
+    //   <Router>
+    //     <Routes>
+    //       <Route path='/'>
+    //       <Route index element={<Home />} />
+    //       <Route path='login' element={<Login />} />
+    //       </Route>
+    //     </Routes>
+    //   </Router>
+    // </div>
     <div>
-    <Login />
-    <SIgnUp />
-    <Home />
+      <Router>
+        <Routes>
+          <Route path='/'>
+          <Route path='login' element={<Login />} />
+        <Route index element={
+          <RequireAuth>
+            <Home />
+          </RequireAuth>
+        } />
+          </Route>
+        </Routes>
+      </Router>
     </div>
   )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase"
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+    const [error, setError] = useState(false);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const navigate = useNavigate();
+    
+    const handleLogin = (e)=> {
+        e.preventDefault();
+
+        signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        
+        // Signed up 
+        const user = userCredential.user; 
+        // dispatch({type:"LOGIN", payload:user})
+        navigate("/")
+        // ...
+      })
+      .catch((error) => {
+        setError(true)
+        // ..
+      });
+    }
+
   return (
     <div className="hero min-h-screen bg-base-200">
   <div className="hero-content flex-col lg:flex-row-reverse">
@@ -9,24 +36,25 @@ const Login = () => {
       <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
     </div>
     <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <form className="card-body">
+      <form className="card-body" onSubmit={handleLogin}>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="email" placeholder="email" className="input input-bordered" required />
+          <input type="email" placeholder="email" className="input input-bordered" required onChange={e=> setEmail(e.target.value)}/>
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
+          <input type="password" placeholder="password" className="input input-bordered" required onChange={e=> setPassword(e.target.value)} />
           <label className="label">
             <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
           </label>
         </div>
         <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
+          <button type="submit" className="btn btn-primary">Login</button>
+          {error && <p className="text-red-500">Something went wrong</p>}
         </div>
       </form>
     </div>
